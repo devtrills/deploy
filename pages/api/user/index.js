63dae67484dda97d8de60ff351caf7bebedda764
devtrills <111649072+devtrills@users.noreply.github.com>
2022-08-26 07:12:58 -0400

    Update index.js
    still updating useUser() call

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -3,14 +3,13 @@
 import Dashboard from '../../../public/src/containers/dashboard/Dashbord';
 import { useUser } from '@auth0/nextjs-auth0/dist/frontend/use-user';
 
-const { user, error, isLoading } = useUser();
-export const getServerSideProps = async function () {
-//   const { user } = req.session
-
+export const getServerSideProps = useUser(async function ({ req, res }) {
+  const { user } = req.session
+  console.log(req);
   if (!user) {
     return {
       redirect: {
-        destination: '/api/auth/login',
+        destination: '/login',
         permanent: false,
       },
     }
@@ -19,7 +18,7 @@ export const getServerSideProps = async function () {
   return {
     props: { user },
   }
-};
+})
 
 const User = ({ user }) => {
   // Show the user. No loading state is required
