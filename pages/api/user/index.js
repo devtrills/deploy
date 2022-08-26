a421cf38dc627542da2d04c51c47a41dfb7194b8
godwin micheals <111649072+devtrills@users.noreply.github.com>
2022-08-26 07:36:04 -0400

    Update index.js
    update again

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -5,15 +5,16 @@ import { useUser } from '@auth0/nextjs-auth0/dist/frontend/use-user';
 import {useRouter} from 'next/router';
 
 const router = useRouter();
-const {user} = userUser();
+const {user} = useUser();
 const User = () => {
     if (!user) {
       router.push('/api/auth/login');
     }else{
-  // Show the user. 
-  return (
-    <Dashboard username={JSON.stringify(user, null, 2)} />
-  )}
+        // Show the user. 
+        return (
+            <Dashboard username={JSON.stringify(user, null, 2)} />
+        )
+    }
 }
 
 export default User;
