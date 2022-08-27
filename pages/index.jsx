dd905d74cdb9d35a2f8917131d491f53049c2580
godwin micheals <111649072+devtrills@users.noreply.github.com>
2022-08-27 00:54:37 -0400

    Update index.jsx
    states used

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,26 +7,29 @@ import Homepage from './home';
 // return <h1>{title ? title : 'Default title'}</h1>;
 // }
 const returnUser = () => {
-  const { user, error, isLoading } = useUser();
   console.log(user);
 
-  return { user, error, isLoading };
+  return ;
 }
 
 export default function Controller () {
+  // user
+  const { user, error, isLoading } = useUser();
   // Hook
-  const [ userObj, setUserObj ] = useState(() => returnUser());
+  const [ userObj, setUserObj ] = useState({user: user, error: error, isLoading: isLoading});
+
+  // setUserObj();
 
   // const { user, error, isLoading } = useUser();
-  console.log(userObj);
-  if (userObj.isLoading) return <div>Loading...</div>;
-  if (userObj.error) return <div>{error.message}</div>;
-  if(userObj.user){
+  console.log(this.state.userObj);
+  if (this.state.userObj.isLoading) return <div>Loading...</div>;
+  if (this.state.userObj.error) return <div>{error.message}</div>;
+  if(this.state.userObj.user){
     return (
-      <Dashboard user={userObj.user} username={JSON.stringify(userObj.user, null, 2)} />
+      <Dashboard user={this.state.userObj.user} username={JSON.stringify(this.state.userObj.user, null, 2)} />
       );
     }
   
   
-    return(<Homepage user={userObj.user} /> );
+    return(<Homepage user={this.state.userObj.user} /> );
 }
