bf2e9335afdd3daa4536d00b32b6c147bcaad529
godwin micheals <111649072+devtrills@users.noreply.github.com>
2022-08-27 01:02:45 -0400

    Update index.jsx
    sdc

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,20 +16,20 @@ export default function Controller () {
   // user
   const { user, error, isLoading } = useUser();
   // Hook
-  const [ userObj, setUserObj ] = useState({user: user, error: error, isLoading: isLoading});
+  const [ userObj, setUserObj ] = useState({ user: null, error: null, isLoading: false});
 
-  // setUserObj();
+  setUserObj({user: user, error: error, isLoading: isLoading});
 
   // const { user, error, isLoading } = useUser();
   console.log(this.state);
-  // if (this.state.isLoading) return <div>Loading...</div>;
-  // if (this.state.error) return <div>{error.message}</div>;
-  // if(this.state.user){
+  // if (userObj.isLoading) return <div>Loading...</div>;
+  // if (userObj.error) return <div>{error.message}</div>;
+  // if(userObj.user){
   //   return (
-  //     <Dashboard user={this.state.user} username={JSON.stringify(this.state.user, null, 2)} />
+  //     <Dashboard user={userObj.user} username={JSON.stringify(userObj.user, null, 2)} />
   //     );
   //   }
   
   
-    return(<Homepage user={this.state} /> );
+    return(<Homepage user={'jh'} /> );
 }
