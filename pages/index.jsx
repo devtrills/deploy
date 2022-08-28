3911f0545be6a9a37a0e3a7b11fc29133fec0c8d
godwin micheals <111649072+devtrills@users.noreply.github.com>
2022-08-28 05:32:08 -0400

    Update index.jsx
    homepage/dashboard

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,20 +6,18 @@ import getUser from './api/hooks/user';
 
 
 export default function Controller () {
-  console.log(getUser().user);
-  // const { user, error, isLoading } = useUser();
-  // if (userObj.isLoading) return <div>Loading...</div>;
-  // if (userObj.error) return <div>{error.message}</div>;
-  // if(userObj.user){
-  //   return (
-  //     <Dashboard user={userObj.user} username={JSON.stringify(userObj.user, null, 2)} />
-  //     );
-  //   }
-  
-  
-    return(
+  console.log();
+  if (getUser().isLoading) return <div>Loading...</div>;
+  if (getUser().error) return <div>{error.message}</div>;
+  if(getUser().user){
+    return (
+      <Dashboard user={userObj.user} username={JSON.stringify(userObj.user, null, 2)} />
+      );
+    }else{
+      return(
         <>
           <Homepage  />
         </>
     );
+    }
 }
