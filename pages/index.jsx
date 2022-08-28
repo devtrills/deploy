9ed5883b2ae61ee68d0a998e3e370d3bc35b7828
godwin micheals <111649072+devtrills@users.noreply.github.com>
2022-08-28 05:35:22 -0400

    Update index.jsx
    b

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-// import Dashboard from '../public/src/containers/dashboard/Dashbord';
+import Dashboard from '../public/src/containers/dashboard/Dashbord';
 import Homepage from './home';
 import getUser from './api/hooks/user';
 
 
 
 export default function Controller () {
-  console.log();
+  
   if (getUser().isLoading) return <div>Loading...</div>;
   if (getUser().error) return <div>{error.message}</div>;
   if(getUser().user){
     return (
-      <Dashboard user={userObj.user} username={JSON.stringify(userObj.user, null, 2)} />
+      <Dashboard user={getUser().user} username={JSON.stringify(getUser().user, null, 2)} />
       );
     }else{
       return(
