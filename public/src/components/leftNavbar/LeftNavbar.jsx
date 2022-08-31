6bfd2cc0b0deefd3a9e69f93562c4967574b5a81
godwin micheals <111649072+devtrills@users.noreply.github.com>
2022-08-30 21:18:03 -0400

    leftNav
    changed leftNav styles to open

--- public/src/components/leftNavbar/LeftNavbar.jsx
+++ public/src/components/leftNavbar/LeftNavbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './leftNavbar.module.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faBookOpen, faCog, faHeart, faRightToBracket, faRocket, faTachometer} from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'next/router';
+import { RiCloseLine, RiMenu3Line } from 'react-icons/ri';
 
 function leftnavbar() {
+    
+  const [toggleMenu, setToggleMenu] = useState(false);
+
     const router = useRouter();
     return(
-        <div className={styles.navcontainer}>
+        <div className={toggleMenu ? styles.closeContainer : styles.navcontainer} >
+            <div>
+            {toggleMenu
+          ? <RiCloseLine style={{background: '#fff', boxShadow: '3px 0px 6px 0px rgba(133, 133, 133, 0.75)', borderRadius: '50%', visibility: 'visible',position: 'absolute', marginLeft: 'calc(200px + .4rem)', marginTop: '.6rem'}} color="#000" size={40} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line  color="#000" size={40} onClick={() => setToggleMenu(true)} />}
             <div className={styles.logo} onClick={() => router.push('/home')}>
                 <h2>Investrill</h2>
             </div>
+            </div>
+
             <div className={styles.wrapper}>
                 <ul>
                     <li>
